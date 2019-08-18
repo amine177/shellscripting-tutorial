b6afd66ea1daf38cec1b29b568dddf178a812d42
aminos <amri.amine@protonmail.com>
2019-08-18 09:12:40 +0100

    restructering

--- wildcards/while.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-INPUT_STRING=start
-while [ "$INPUT_STRING" != "fin" ]
-do
-  printf "Enter a string or fin to end\n"
-  read -r INPUT_STRING
-  printf "INPUT: %s\n" "$INPUT_STRING"
-done
